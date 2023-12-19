b78bc45afed8d4d08cb992b9975f6520c518ba69
Amos Elmaliah <amosel@gmail.com>
2023-12-19 12:13:20 -0500

    WIP Apple Login Button

--- src/auth/AppleLoginButton.tsx
+++ src/auth/AppleLoginButton.tsx
@@ -1,15 +1,15 @@
-import { Text } from '~/design/typography'
+import { View, Text, Platform } from 'react-native'
 import { ButtonContainerA } from '~/design/button'
 import { FontAwesome } from '@expo/vector-icons'
+import type { AppleAuthenticationCredential } from 'expo-apple-authentication'
 import {
   signInAsync,
   AppleAuthenticationScope,
-  AppleAuthenticationCredential,
   // AppleAuthenticationFullName
 } from 'expo-apple-authentication'
-import { View } from '~/design/view'
-import { Platform } from 'react-native'
-import { } from 'expo-secure-store'
+// import { Platform } from 'react-native'
+import ExpoSecureStorage from 'expo-secure-store'
+import { useState } from 'react'
 // import { z } from 'zod';
 
 // const cancelled = z.object({
@@ -30,7 +30,20 @@ Be warned!
 The email and fullName will only be populated ONCE. The first time they press the button, this applies even if they change their device or update the app. A user's email is still available in the JWT token, but once fullName gets pulled down the first time, you can never request it again and subsequent requests will return null.
 */
 
+const Cache = {
+  getAppleLoginName: (username: string) =>
+    ExpoSecureStorage.getItemAsync(`login/${username}`),
+}
+
+async function createAccount(
+  _cachedName: string,
+  _user: string,
+  _identityToken: string,
+) {}
+async function login(_identityToken: string) {}
+
 export const AppleLoginButton = () => {
+  const [_, onError] = useState<string>()
   if (Platform.OS !== 'ios') {
     return null
   }
@@ -45,20 +58,24 @@ export const AppleLoginButton = () => {
               AppleAuthenticationScope.EMAIL,
             ],
           })
-          const cachedName: string = await Cache.getAppleLoginName(
-            credential.user,
-          )
+          const cachedName = await Cache.getAppleLoginName(credential.user)
           const givenName = credential.fullName?.givenName
           const detailsArePopulated: boolean = !!givenName && !!credential.email
           if (!detailsArePopulated && !cachedName) {
-            await login(credential.identityToken)
+            await login(credential.identityToken!)
           } else if (!detailsArePopulated && cachedName) {
             await createAccount(
               cachedName,
               credential.user,
-              credential.identityToken,
+              credential.identityToken!,
             )
           } else {
+            if (!givenName) {
+              throw Error('Given Name is null')
+            }
+            if (!credential.identityToken) {
+              throw Error('Given Name is null')
+            }
             await createAccount(
               givenName,
               credential.user,
@@ -66,10 +83,15 @@ export const AppleLoginButton = () => {
             )
           }
         } catch (error) {
-          if (error.code === 'ERR_CANCELED') {
+          if (
+            typeof error === 'object' &&
+            !!error &&
+            'code' in error &&
+            error.code === 'ERR_CANCELED'
+          ) {
             onError('Continue was cancelled.')
           } else {
-            onError(error.message)
+            onError((error as { messaage: string }).messaage)
           }
         }
       }}>
