c74c2ebe815ed86c00344e33ed5ed7d9984a5451
Amos Elmaliah <amosel@gmail.com>
2023-12-19 11:08:14 -0500

    removed unused file.

--- src/auth/AuthButtonContainer.tsx
+++
@@ -1,8 +0,0 @@
-import { TouchableOpacity } from "~/design/button"
-
-export const AuthButtonContainer = ({ children }) => (
-  <TouchableOpacity
-  onPress={onLogin}
-  className='w-4/5 flex-row h-12 snap-center justify-center rounded-xl my-3 bg-black'>
->{children}</TouchableOpacity>
-)
